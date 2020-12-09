137dd044f6c1e16d55d3702071f59809640559cf
Tirzo <ttejeda@ucol.mx>
2020-12-09 12:23:59 -0600

    implemented sumatoriaSerieUno()

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
-export default class App {}
+export default class App {
+
+    sumatoriaSerieUno(numero){
+        let suma = 0;
+
+        for(let i = 1; i <= numero; i = i + 1){
+            let fraccion = 1 / i;
+            suma = suma + fraccion;
+            fraccion = 0;
+        }
+
+      return suma;
+    }
+}
 
 let app = new App();
+
+console.log(app.sumatoriaSerieUno(9));
