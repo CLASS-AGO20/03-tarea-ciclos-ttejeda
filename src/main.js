50fd00baeb83e8cf63484161d3898c30ea378401
Tirzo <ttejeda@ucol.mx>
2020-12-11 11:00:38 -0600

    added missing test files

--- src/main.js
+++ src/main.js
@@ -27,6 +27,28 @@ export default class App {
 
       return suma;
     }
+
+    esPrimo(numero){
+        let i = 1;
+        let p;
+
+        do {
+        
+          if (numero % i == 0){
+           p = p + 1;
+          }
+
+          i = i + 1;
+        } while (i <= numero)
+
+        if(p > 2){
+          p = false;
+        } else {
+          p = true;
+        }
+
+      return p;
+    }
 }
 
 let app = new App();
@@ -34,3 +56,6 @@ let app = new App();
 console.log(app.sumatoriaSerieUno(9));
 
 console.log(app.sumatoriaSerieDos(9));
+
+console.log(app.esPrimo(2));
+console.log(app.esPrimo(77));
